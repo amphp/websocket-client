8b41165ca60358068a378009e5e7a203bb992aa1
Aaron Piotrowski <aaron@trowski.com>
2017-03-24 11:20:48 -0500

    connect → rawConnect

--- lib/functions.php
+++ lib/functions.php
@@ -4,17 +4,25 @@ namespace Amp\Websocket;
 
 use Amp\{ Promise, Socket, UnionTypeError };
 
-function connect($handshake, array $options = []) {
-    if (is_string($handshake)) {
+/**
+ * @param string|\Amp\WebSocket\Handshake $handshake
+ * @param array $options Connect options. See \Amp\Socket\connect() and \Amp\Socket\cryptoConnect().
+ *
+ * @return \Amp\Promise<\Amp\WebSocket\Connection>
+ *
+ * @throws \Amp\UnionTypeError If $handshake is not a string or instance of \Amp\WebSocket\Handshake.
+ */
+function connect($handshake, array $options = []): Promise {
+    if (\is_string($handshake)) {
         $handshake = new Handshake($handshake);
     } elseif (!$handshake instanceof Handshake) {
         throw new UnionTypeError(["string", Handshake::class], $handshake);
     }
 
     if ($handshake->hasCrypto()) {
-        $promise = Socket\cryptoConnect($handshake->getTarget(), $options);
+        $promise = Socket\rawCryptoConnect($handshake->getTarget(), $options);
     } else {
-        $promise = Socket\connect($handshake->getTarget(), $options);
+        $promise = Socket\rawConnect($handshake->getTarget(), $options);
     }
 
     return Promise\pipe($promise, [$handshake, 'send']);
