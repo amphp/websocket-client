33ecc62bc291dd794f2cd58b750283cdee088de7
Niklas Keller <me@kelunik.com>
2017-12-15 19:38:32 +0100

    Remove logic from Handshake and move it into connect

--- lib/Handshake.php
+++ lib/Handshake.php
@@ -2,110 +2,67 @@
 
 namespace Amp\Websocket;
 
-use Amp\Promise;
-use Amp\Socket\Socket;
-use function Amp\call;
-
-class Handshake {
-    const ACCEPT_CONCAT = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+final class Handshake {
+    const ACCEPT_CONCAT = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
     const ACCEPT_NONCE_LENGTH = 12;
 
-    private $crypto;
-    private $target;
+    private $encrypted;
+    private $remoteAddress;
     private $path;
     private $headers = [];
-    private $options;
 
     /**
      * @param string $url target address of websocket (e.g. ws://foo.bar/baz or wss://crypto.example/?secureConnection)
-     * @param mixed[] $options Connection options
      */
-    public function __construct(string $url, array $options = []) {
-        $this->options = $options;
-
+    public function __construct(string $url) {
         $url = \parse_url($url);
 
-        $this->crypto = $url["scheme"] === "wss";
-        $defaultPort = $this->crypto ? 443 : 80;
+        $this->encrypted = $url['scheme'] === 'wss';
+        $defaultPort = $this->encrypted ? 443 : 80;
 
-        $host = $url["host"];
-        $port = $url["port"] ?? $defaultPort;
+        $host = $url['host'];
+        $port = $url['port'] ?? $defaultPort;
 
-        $this->target = $host . ":" . $port;
+        $this->remoteAddress = $host . ':' . $port;
 
-        if ($url["port"] !== $defaultPort) {
-            $host .= ":" . $port;
+        if ($url['port'] !== $defaultPort) {
+            $host .= ':' . $port;
         }
 
-        $this->headers["Host"] = $host;
-        $this->path = $url["path"] ?? "/";
+        $this->headers['host'][] = $host;
+        $this->path = $url['path'] ?? '/';
 
-        if (isset($url["query"])) {
+        if (isset($url['query'])) {
             $this->path .= "?{$url['query']}";
         }
     }
 
-    public function setHeader(string $field, $value): self {
-        $this->headers[$field] = $value;
+    public function addHeader(string $field, string $value): self {
+        $this->headers[$field][] = $value;
 
         return $this;
     }
 
-    public function getTarget(): string {
-        return $this->target;
+    public function getRemoteAddress(): string {
+        return $this->remoteAddress;
     }
 
-    public function hasCrypto(): bool {
-        return $this->crypto;
+    public function isEncrypted(): bool {
+        return $this->encrypted;
     }
 
-    private function getRequest(): string {
-        $headers = "";
+    public function getRawRequest(): string {
+        $headers = '';
 
-        foreach ($this->headers as $field => $value) {
-            $headers .= "$field: $value\r\n";
+        foreach ($this->headers as $field => $values) {
+            /** @var array $values */
+            foreach ($values as $value) {
+                $headers .= "$field: $value\r\n";
+            }
         }
 
         $accept = \base64_encode(\random_bytes(self::ACCEPT_NONCE_LENGTH));
 
         return "GET $this->path HTTP/1.1\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-Websocket-Version: 13\r\nSec-Websocket-Key: $accept\r\n$headers\r\n";
-    }
-
-    public function send(Socket $socket): Promise {
-        return call(function () use ($socket) {
-            $data = $this->getRequest();
-
-            yield $socket->write($data);
-
-            $buffer = "";
-
-            while (($chunk = yield $socket->read()) !== null) {
-                $buffer .= $chunk;
-
-                if ($position = \strpos($buffer, "\r\n\r\n")) {
-                    $header = \substr($buffer, 0, $position + 4);
-
-                    return $this->createConnection($socket, $header, \substr($buffer, $position + 4));
-                }
-            }
-
-            throw new ServerException("Failed to read response from server");
-        });
-    }
-
-    private function createConnection($socket, string $data, string $buffer): Connection {
-        if (!\preg_match("(^HTTP/1.1[\x20\x09]101[\x20\x09]*[^\x01-\x08\x10-\x19]*$)", \substr($data, 0, \strpos($data, "\r\n")))) {
-            throw new ServerException("Did not receive switching protocols response");
-        }
-        \preg_match_all("(
-            (?P<field>[^()<>@,;:\\\"/[\\]?={}\x01-\x20\x7F]+):[\x20\x09]*
-            (?P<value>[^\x01-\x08\x0A-\x1F\x7F]*)[\x0D]?[\x20\x09]*[\r]?[\n]
-        )x", $data, $m);
-        $headers = [];
-        foreach ($m["field"] as $idx => $field) {
-            $headers[\strtolower($field)][] = $m["value"][$idx];
-        }
-        // TODO: validate headers...
-        return new Rfc6455Connection($socket, $headers, $buffer, $this->options);
     }
 }
--- lib/functions.php
+++ lib/functions.php
@@ -5,32 +5,69 @@ namespace Amp\Websocket;
 use Amp\Promise;
 use Amp\Socket;
 use Amp\Socket\ClientConnectContext;
+use Amp\Socket\ClientSocket;
 use Amp\Socket\ClientTlsContext;
 use function Amp\call;
 
 /**
- * @param string|\Amp\WebSocket\Handshake $handshake
+ * @param string|\Amp\Websocket\Handshake       $handshake
  * @param \Amp\Socket\ClientConnectContext|null $connectContext
- * @param \Amp\Socket\ClientTlsContext|null $tlsContext
+ * @param \Amp\Socket\ClientTlsContext|null     $tlsContext
+ * @param array                                 $options
  *
  * @return \Amp\Promise<\Amp\WebSocket\Connection>
  *
  * @throws \TypeError If $handshake is not a string or instance of \Amp\WebSocket\Handshake.
  */
-function connect($handshake, ClientConnectContext $connectContext = null, ClientTlsContext $tlsContext = null): Promise {
+function connect($handshake, ClientConnectContext $connectContext = null, ClientTlsContext $tlsContext = null, array $options = []): Promise {
     if (\is_string($handshake)) {
         $handshake = new Handshake($handshake);
     } elseif (!$handshake instanceof Handshake) {
-        throw new \TypeError(\sprintf("Must provide an instance of %s or a URL as a string", Handshake::class));
+        throw new \TypeError(\sprintf('Must provide an instance of %s or a URL as a string', Handshake::class));
     }
 
-    return call(function () use ($handshake, $connectContext, $tlsContext) {
-        if ($handshake->hasCrypto()) {
-            $socket = yield Socket\cryptoConnect($handshake->getTarget(), $connectContext, $tlsContext);
+    return call(function () use ($handshake, $connectContext, $tlsContext, $options) {
+        if ($handshake->isEncrypted()) {
+            /** @var ClientSocket $socket */
+            $socket = yield Socket\cryptoConnect($handshake->getRemoteAddress(), $connectContext, $tlsContext);
         } else {
-            $socket = yield Socket\connect($handshake->getTarget(), $connectContext);
+            /** @var ClientSocket $socket */
+            $socket = yield Socket\connect($handshake->getRemoteAddress(), $connectContext);
         }
 
-        return yield $handshake->send($socket);
+        yield $socket->write($handshake->getRawRequest());
+
+        $buffer = '';
+
+        while (($chunk = yield $socket->read()) !== null) {
+            $buffer .= $chunk;
+
+            if ($position = \strpos($buffer, "\r\n\r\n")) {
+                $headerBuffer = \substr($buffer, 0, $position + 4);
+                $buffer = \substr($buffer, $position + 4);
+
+                if (!\preg_match("(^HTTP/1.1[\x20\x09]101[\x20\x09]*[^\x01-\x08\x10-\x19]*$)", \substr($headerBuffer, 0, \strpos($headerBuffer, "\r\n")))) {
+                    throw new ServerException('Did not receive switching protocols response');
+                }
+
+                \preg_match_all("(
+                    (?P<field>[^()<>@,;:\\\"/[\\]?={}\x01-\x20\x7F]+):[\x20\x09]*
+                    (?P<value>[^\x01-\x08\x0A-\x1F\x7F]*)\x0D?[\x20\x09]*\r?\n
+                )x", $headerBuffer, $responseHeaders);
+
+                $headers = [];
+
+                /** @var array[] $responseHeaders */
+                foreach ($responseHeaders['field'] as $idx => $field) {
+                    $headers[\strtolower($field)][] = $responseHeaders['value'][$idx];
+                }
+
+                // TODO: validate headers...
+
+                return new Rfc6455Connection($socket, $headers, $buffer, $options);
+            }
+        }
+
+        throw new ServerException('Failed to read response from server');
     });
 }
