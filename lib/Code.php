7135398d05565823f5e2a6ace41f77b3dc27f1ca
Niklas Keller <me@kelunik.com>
2017-12-15 18:54:34 +0100

    Make Code final

--- lib/Code.php
+++ lib/Code.php
@@ -2,7 +2,7 @@
 
 namespace Amp\Websocket;
 
-class Code {
+final class Code {
     const NORMAL_CLOSE = 1000;
     const GOING_AWAY = 1001;
     const PROTOCOL_ERROR = 1002;
@@ -15,4 +15,8 @@ class Code {
     const EXPECTED_EXTENSION_MISSING = 1010;
     const UNEXPECTED_SERVER_ERROR = 1011;
     const TLS_HANDSHAKE_FAILURE = 1015;
-};
+
+    private function __construct() {
+        // no instances allowed
+    }
+}
