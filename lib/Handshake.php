97f7a0a08734158d0014db0d8bdb383e5d77f813
Niklas Keller <me@kelunik.com>
2017-12-17 16:56:22 +0100

    Correctly validate handshake headers and fix nonce length

--- lib/Handshake.php
+++ lib/Handshake.php
@@ -3,13 +3,15 @@
 namespace Amp\Websocket;
 
 final class Handshake {
+    // defined in https://tools.ietf.org/html/rfc6455#section-4
     const ACCEPT_CONCAT = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-    const ACCEPT_NONCE_LENGTH = 12;
+    const ACCEPT_NONCE_LENGTH = 16;
 
     private $encrypted;
     private $remoteAddress;
     private $path;
     private $headers = [];
+    private $accept;
 
     /**
      * @param string $url target address of websocket (e.g. ws://foo.bar/baz or wss://crypto.example/?secureConnection)
@@ -65,9 +67,11 @@ final class Handshake {
             }
         }
 
-        $accept = \base64_encode(\random_bytes(self::ACCEPT_NONCE_LENGTH));
+        if ($this->accept === null) {
+            $this->accept = \base64_encode(\random_bytes(self::ACCEPT_NONCE_LENGTH));
+        }
 
-        return 'GET ' . $this->path . " HTTP/1.1\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-Websocket-Version: 13\r\nSec-Websocket-Key: $accept\r\n$headers\r\n";
+        return 'GET ' . $this->path . " HTTP/1.1\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-Websocket-Version: 13\r\nSec-Websocket-Key: {$this->accept}\r\n$headers\r\n";
     }
 
     public function decodeResponse(string $headerBuffer): array {
@@ -89,7 +93,22 @@ final class Handshake {
             $headers[\strtolower($field)][] = $responseHeaders['value'][$idx];
         }
 
-        // TODO: validate headers...
+        $upgrade = $headers['upgrade'][0] ?? '';
+        if (\strtolower($upgrade) !== 'websocket') {
+            throw new WebSocketException('Missing "Upgrade: websocket" header.');
+        }
+
+        $connection = $headers['connection'][0] ?? '';
+        if (!\in_array('upgrade', array_map('trim', array_map('strtolower', explode(',', $connection))), true)) {
+            throw new WebSocketException('Missing "Connection: upgrade" header.');
+        }
+
+        $secWebsocketAccept = $headers['sec-websocket-accept'][0] ?? '';
+        if ($secWebsocketAccept !== \base64_encode(\sha1($this->accept . self::ACCEPT_CONCAT, true))) {
+            throw new WebSocketException('Invalid "Sec-WebSocket-Accept" header.');
+        }
+
+        // TODO: Check extension header and fail handshake if extensions are not supported
 
         return $headers;
     }
