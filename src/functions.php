8b166836eb2284b391a7a03bb4a2de91c27ee731
Niklas Keller <me@kelunik.com>
2020-07-04 18:53:18 +0200

    Remove possibility to change global connector
    If you need customization, better depend on the Connector interface or Rfc6455Connector directly.

--- src/functions.php
+++ src/functions.php
@@ -5,33 +5,8 @@ namespace Amp\Websocket\Client;
 use Amp\CancellationToken;
 use Amp\Http\Client\HttpClientBuilder;
 use Amp\Http\Client\HttpException;
-use Amp\Loop;
 use Amp\Promise;
 use Psr\Http\Message\UriInterface as PsrUri;
-
-const LOOP_CONNECTOR_IDENTIFIER = Connector::class;
-
-/**
- * Set or access the global websocket Connector instance.
- *
- * @param Connector|null $connector
- *
- * @return Connector
- */
-function connector(?Connector $connector = null): Connector
-{
-    if ($connector === null) {
-        $connector = Loop::getState(LOOP_CONNECTOR_IDENTIFIER);
-        if ($connector) {
-            return $connector;
-        }
-
-        $connector = new Rfc6455Connector(HttpClientBuilder::buildDefault());
-    }
-
-    Loop::setState(LOOP_CONNECTOR_IDENTIFIER, $connector);
-    return $connector;
-}
 
 /**
  * @param string|PsrUri|Handshake $handshake
@@ -49,5 +24,5 @@ function connect($handshake, ?CancellationToken $cancellationToken = null): Prom
         $handshake = new Handshake($handshake);
     }
 
-    return connector()->connect($handshake, $cancellationToken);
+    return (new Rfc6455Connector(HttpClientBuilder::buildDefault()))->connect($handshake, $cancellationToken);
 }
